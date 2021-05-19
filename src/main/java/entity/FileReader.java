845ff2de7eadba1c91d362df6a63d89d4acd1343
kiendao515 <daotrungkien515@gmail.com>
2021-05-19 22:14:09 +0700

    fix bug

--- src/main/java/entity/FileReader.java
+++ src/main/java/entity/FileReader.java
@@ -1,21 +1,9 @@
 package entity;
 
-import org.graphstream.graph.Edge;
-import org.graphstream.graph.Graph;
-import org.graphstream.graph.Node;
-import org.graphstream.graph.implementations.SingleGraph;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class FileReader {
     public static int NumberNode= 0;
     public static final int MAXN= 10000005;
     public static Point point[]= new Point[MAXN];
-    public static int trace[]= new int[MAXN];
-
     public static void convert(String s) {
         int index= 0, node, next;
         String string= "";
@@ -25,12 +13,10 @@ public class FileReader {
         NumberNode= Math.max(NumberNode, node);
         string= "";
         ++index;
-        //point[node]= new Point(node);
 
         while(index<= s.length()){
             if(index== s.length()|| s.charAt(index)== ' '){
                 next= Integer.parseInt(string);
-                //graph.addEdge(node+""+next,String.valueOf(node),string);
                 NumberNode= Math.max(NumberNode, next);
                 point[next].getList().add(node);
                 ++index;
@@ -39,31 +25,7 @@ public class FileReader {
             }
             string+= s.charAt(index++);
         }
-        //graph.display();
     }
 
-    static void Print(int index)
-    {
-        while(true){
-            System.out.print(index+ " ");
-            index= trace[index];
-            if(index== NumberNode) break;
-        }
-        System.out.println(NumberNode);
-    }
 
-    static void DFS(int index)
-    {
-        if(index== 1){
-            Print(1);
-            return;
-        }
-        for(Integer node: point[index].getList())
-        {
-            if(trace[node]!= 0) continue;
-            trace[node]= index;
-            DFS(node);
-            trace[node]= 0;
-        }
-    }
 }
--- src/main/java/service/ReadFile.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-public interface ReadFile {
-     void convert(String s);
-    void print(int index);
-    void DFS(int index);
-
-}
